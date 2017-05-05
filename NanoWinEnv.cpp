4a0fa1924254af063af4ff9791a44fbcb6b03d72
S.Ageyev <justboss777@gmail.com>
2017-05-05 19:57:15 +0300

    Initial implementation of environment funcs

---
+++ NanoWinEnv.cpp
@@ -0,0 +1,42 @@
+// NanoWin32
+// -----------------------------------------------------------------------
+// Simple library to subset Win32(64) API functions implemenation on POSIX
+// This software distributed by MIT license
+
+// Environment functions
+
+#include "NanoWinEnv.h"
+
+#include <string.h>
+#include <stdlib.h>
+
+#include "NanoMsSafeString.h"
+
+EXTERN_C_BEGIN
+
+DWORD GetEnvironmentVariableA
+              (
+                LPCSTR lpName,
+                LPSTR  lpBuffer,
+                DWORD  nSize
+              )
+{
+  if ((nSize > 0) && (lpBuffer != NULL))
+  {
+    lpBuffer[0] = '\0';
+  }
+
+  LPCSTR result = getenv(lpName);
+  if (result == NULL)
+  {
+    return(0);
+  }
+
+  int resultlen = strlen(result);
+
+  strcpy_s(lpBuffer, nSize, result);
+
+  return(resultlen);
+}
+
+EXTERN_C_END
---
+++ NanoWinEnv.h
@@ -0,0 +1,33 @@
+// NanoWin32
+// -----------------------------------------------------------------------
+// Simple library to subset Win32(64) API functions implemenation on POSIX
+// This software distributed by MIT license
+
+// Environment functions
+
+#if !defined(NanoWinEnvIncluded)
+#define NanoWinEnvIncluded
+
+#include "NanoWinTypes.h"
+
+#if defined LINUX
+
+EXTERN_C_BEGIN
+
+extern  DWORD GetEnvironmentVariableA
+              (
+                LPCSTR lpName,
+                LPSTR  lpBuffer,
+                DWORD  nSize
+              );
+
+#if defined(UNICODE)
+#define	GetEnvironmentVariable	GetEnvironmentVariableW // TODO: Impement me
+#else
+#define	GetEnvironmentVariable	GetEnvironmentVariableA
+#endif
+
+EXTERN_C_END
+
+#endif // linux
+#endif // ...Included
