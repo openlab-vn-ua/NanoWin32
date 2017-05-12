8b3648536b89028050d83ecc256919b86a36b149
S.Ageyev <justboss777@gmail.com>
2017-05-12 22:27:42 +0300

    Initial Implementation (as stub)

--- NanoWinFileFind.h
+++ NanoWinFileFind.h
@@ -9,13 +9,26 @@
 #define NanoWinFileFindIncluded
 
 #include "NanoWinTypes.h"
+#include "NanoWinFileSys.h"
 
 #if defined LINUX
 
+struct WIN32_FIND_DATAA
+{
+  char cFileName[MAX_PATH]; // _MAX_FNAME ?
+};
+
 NW_EXTERN_C_BEGIN
+
+inline HANDLE FindFirstFileA(const char *Mask, WIN32_FIND_DATAA *state) { return INVALID_HANDLE_VALUE; }
+inline BOOL   FindNextFileA(HANDLE handle, WIN32_FIND_DATAA *state) { return FALSE; }
+inline BOOL   FindClose(HANDLE hFindFile) { return TRUE; }
 
 #if defined(UNICODE) || defined(_UNICODE)
 #else
+#define WIN32_FIND_DATA WIN32_FIND_DATAA
+#define FindFirstFile   FindFirstFileA
+#define FindNextFile    FindNextFileA
 #endif
 
 NW_EXTERN_C_END
