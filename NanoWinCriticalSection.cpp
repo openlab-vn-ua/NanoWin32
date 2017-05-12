6cedc75be09aafb4c40bef322d6af13304ead3c1
S.Ageyev <justboss777@gmail.com>
2017-05-12 22:23:43 +0300

    Initial implementation

---
+++ NanoWinCriticalSection.cpp
@@ -0,0 +1,45 @@
+// NanoWin32
+// -----------------------------------------------------------------------
+// Simple library to subset Win32(64) API functions implemenation on POSIX
+// This software distributed by MIT license
+
+// CriticalSection functions
+
+#include "NanoWinCriticalSection.h"
+
+//#include "NanoWinError.h"
+
+NW_EXTERN_C_BEGIN
+
+static void log_error(int error_code, const char *func_name)
+{
+  // NanoWin::log(func_name, " Unexpected error in CRITICAL_SECTION:", error_code);
+}
+
+#define check_error(expr) { int err = (expr); if (err != 0) { log_error(err, #expr); } }
+
+void InitializeCriticalSection(LPCRITICAL_SECTION lpCriticalSection)
+{
+  check_error(pthread_mutexattr_init(&lpCriticalSection->mutexattr));
+  check_error(pthread_mutexattr_settype(&lpCriticalSection->mutexattr, PTHREAD_MUTEX_NORMAL));
+  check_error(pthread_mutex_init(&lpCriticalSection->mutex, &lpCriticalSection->mutexattr));
+}
+
+void EnterCriticalSection(LPCRITICAL_SECTION lpCriticalSection)
+{
+  check_error(pthread_mutex_lock(&lpCriticalSection->mutex));
+}
+
+void LeaveCriticalSection(LPCRITICAL_SECTION lpCriticalSection)
+{
+  check_error(pthread_mutex_unlock(&lpCriticalSection->mutex));
+}
+
+void DeleteCriticalSection(LPCRITICAL_SECTION lpCriticalSection)
+{
+  check_error(pthread_mutex_destroy(&lpCriticalSection->mutex));
+  check_error(pthread_mutexattr_destroy(&lpCriticalSection->mutexattr));
+}
+
+
+NW_EXTERN_C_END
---
+++ NanoWinCriticalSection.h
@@ -0,0 +1,35 @@
+// NanoWin32
+// -----------------------------------------------------------------------
+// Simple library to subset Win32(64) API functions implemenation on POSIX
+// This software distributed by MIT license
+
+// CriticalSection functions
+
+#if !defined(NanoWinCriticalSectionIncluded)
+#define NanoWinCriticalSectionIncluded
+
+#include "NanoWinTypes.h"
+
+#if defined LINUX
+
+#include <pthread.h>
+
+typedef struct
+{
+  pthread_mutex_t     mutex;
+  pthread_mutexattr_t mutexattr;
+} CRITICAL_SECTION;
+typedef CRITICAL_SECTION *LPCRITICAL_SECTION;
+
+
+NW_EXTERN_C_BEGIN
+
+extern void InitializeCriticalSection (LPCRITICAL_SECTION lpCriticalSection);
+extern void EnterCriticalSection      (LPCRITICAL_SECTION lpCriticalSection);
+extern void LeaveCriticalSection      (LPCRITICAL_SECTION lpCriticalSection);
+extern void DeleteCriticalSection     (LPCRITICAL_SECTION lpCriticalSection);
+
+NW_EXTERN_C_END
+
+#endif // linux
+#endif // ...Included
