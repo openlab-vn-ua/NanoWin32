222bad51a68816e30a069ab9dbfc9831bf4a31b9
D. Antonov <dendelit@gmail.com>
2017-05-13 21:57:48 +0300

    Micro stubs to MFC App classes (just to compile)

---
+++ NanoWinMFCApp.cpp
@@ -0,0 +1,15 @@
+// NanoWin32
+// -----------------------------------------------------------------------
+// Simple library to subset Win32(64) API functions implemenation on POSIX
+// This software distributed by MIT license
+
+// MFCApp functions
+
+#include "NanoWinMFCApp.h"
+
+#include "NanoWinError.h"
+
+NW_EXTERN_C_BEGIN
+
+
+NW_EXTERN_C_END
---
+++ NanoWinMFCApp.h
@@ -0,0 +1,21 @@
+// NanoWin32
+// -----------------------------------------------------------------------
+// Simple library to subset Win32(64) API functions implemenation on POSIX
+// This software distributed by MIT license
+
+// MFCApp functions
+// Micro stubs to allow code just to compile for command line run (no UI)
+
+#if !defined(NanoWinMFCAppIncluded)
+#define NanoWinMFCAppIncluded
+
+#include "NanoWinTypes.h"
+
+#if defined LINUX
+
+class CWinApp { };
+
+#define AfxWinInit(a,b,c,d) (TRUE)
+
+#endif // linux
+#endif // ...Included
