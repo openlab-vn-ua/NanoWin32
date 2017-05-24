13b40069feedd98eb441adc42fcdb3b45da8f005
Anastasia Tkachuk <1pi15b.tkachuk@gmail.com>
2017-05-24 17:49:25 +0300

    added tests for FileCopy

--- tests/src/NanoWinFileSysTests.cpp
+++ tests/src/NanoWinFileSysTests.cpp
@@ -2,11 +2,46 @@
 
 #ifdef __linux
  #include "unistd.h"
+ #include <sys/types.h>
+ #include <sys/stat.h>
  #include "NanoWinFileSys.h"
 #else
  #error "unsupported platform"
 // #include <windows.h>
 #endif
+
+static void CreateFile(const int size, const char *fileName, const char symbol)
+{
+	FILE *file;
+
+	file = fopen(fileName, "w");
+
+	if (file != NULL)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			fputc(symbol, file);
+		}
+	}
+
+	fclose(file);
+}
+
+static void RemoveTestDir()
+{
+	unlink("testDir/testFile256.txt");
+	unlink("testDir/testFile256-copy.txt");
+	unlink("testDir/testFile512.txt");
+	rmdir("testDir");
+}
+
+static void CreateTestDir()
+{
+	mkdir("testDir", S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+
+	CreateFile(512, "testDir/testFile512.txt", 'a');
+	CreateFile(256, "testDir/testFile256.txt", 'a');
+}
 
 NW_BEGIN_TEST_GROUP_SIMPLE(NanoWinFileSysGetCurrDirTestGroup)
 
@@ -98,6 +133,83 @@ NW_TEST(NanoWinFileSysGetCurrDirTestGroup, GetCurrentDirectoryWSmallBufferSizeTe
   NW_CHECK_EQUAL_ULONGS(5, result);
 
   NW_CHECK(chdir(initialDir) == 0);
+}
+
+NW_END_TEST_GROUP()
+
+NW_BEGIN_TEST_GROUP(NanoWinFileSysCopyFileATestGroup)
+
+NW_BEGIN_SETUP_TEARDOWN
+
+NW_SETUP_METHOD()
+{
+  CreateTestDir();
+}
+
+NW_TEARDOWN_METHOD()
+{
+  RemoveTestDir();
+}
+
+NW_END_SETUP_TEARDOWN
+
+NW_TEST(NanoWinFileSysCopyFileATestGroup, UnexistingFileCopyTest)
+{
+  int    copyRes = CopyFileA("testDir/unexistFile.txt", "testDir/testFile111.txt", FALSE);
+
+  NW_CHECK(copyRes == 0);
+  
+  struct stat statFileUnexist;
+  int    statRes = stat("testDir/testFile111.txt", &statFileUnexist);
+
+  NW_CHECK_EQUAL(-1, statRes);
+}
+
+NW_TEST(NanoWinFileSysCopyFileATestGroup, FileCopySimpleTest)
+{
+  struct stat statFileSource;
+  struct stat statFileCopy;
+
+  chmod("testDir/testFile256.txt", S_IRUSR | S_IWUSR | S_IRGRP);
+
+  int copyRes = CopyFileA("testDir/testFile256.txt", "testDir/testFile256-copy.txt", FALSE);
+
+  NW_CHECK(copyRes != 0);
+
+  stat("testDir/testFile256.txt",      &statFileSource);
+  stat("testDir/testFile256-copy.txt", &statFileCopy);
+
+  NW_CHECK_EQUAL_BYTES(statFileSource.st_size, statFileCopy.st_size);
+
+  NW_CHECK_EQUAL(statFileSource.st_uid,  statFileCopy.st_uid);
+  NW_CHECK_EQUAL(statFileSource.st_gid,  statFileCopy.st_gid);
+  NW_CHECK_EQUAL(statFileSource.st_mode, statFileCopy.st_mode);
+}
+
+NW_TEST(NanoWinFileSysCopyFileATestGroup, FileCopyExistingFileNotRewriteTest)
+{
+  int copyRes = CopyFileA("testDir/testFile256.txt", "testDir/testFile512.txt", TRUE);
+
+  NW_CHECK(copyRes == 0);
+}
+
+NW_TEST(NanoWinFileSysCopyFileATestGroup, FileCopyExistingFileRewritingTest)
+{
+  struct stat statFileSource;
+  struct stat statFileCopy;
+
+  int copyRes = CopyFileA("testDir/testFile256.txt", "testDir/testFile512.txt", FALSE);
+
+  NW_CHECK(copyRes != 0);
+
+  stat("testDir/testFile256.txt", &statFileSource);
+  stat("testDir/testFile512.txt", &statFileCopy);
+
+  NW_CHECK_EQUAL_BYTES(statFileSource.st_size, statFileCopy.st_size);
+
+  NW_CHECK_EQUAL(statFileSource.st_uid,  statFileCopy.st_uid);
+  NW_CHECK_EQUAL(statFileSource.st_gid,  statFileCopy.st_gid);
+  NW_CHECK_EQUAL(statFileSource.st_mode, statFileCopy.st_mode);
 }
 
 NW_END_TEST_GROUP()
