559b3fb91bc143fd7e3cab526dd4be07db24432e
Sergey Prokhorchuk <s.stager@gmail.com>
2017-05-25 21:03:07 +0300

    CArray implementation added

--- src/NanoWinMFCAfxCol.h
+++ src/NanoWinMFCAfxCol.h
@@ -12,6 +12,7 @@
 
 #if defined LINUX
 
+#include <vector>
 // MFC subset (collection classes)
 
 #include "NanoWinMFCAfx.h"
@@ -27,35 +28,127 @@ typedef __POSITION* POSITION;
 template <class TYPE, class ARG_TYPE = const TYPE&>
 class CArray : public CObject
 {
+  #define REQUIRE(f) if (!(f)) { throw "CArray has invalid param:" #f; }
+
   private:
-  TYPE *data;
+//  TYPE *data;
+
+  typedef std::vector<TYPE> storage_type;
+
+  storage_type storage;
 
   protected:
   int m_nSize; // Invisble for children some reason?
 
   public:
-  TYPE& GetAt(INT_PTR nIndex);
-  const TYPE& GetAt(INT_PTR nIndex) const;
 
-  INT_PTR GetSize() const;
-  INT_PTR GetCount() const;
+  TYPE& GetAt(INT_PTR nIndex)
+  {
+    REQUIRE(nIndex >= 0);
+    REQUIRE(nIndex < GetSize());
 
-  void SetSize(INT_PTR nNewSize,INT_PTR nGrowBy = -1);
+    return storage[nIndex];
+  }
 
-  INT_PTR Add(ARG_TYPE newElement);
+  const TYPE& GetAt(INT_PTR nIndex) const
+  {
+    REQUIRE(nIndex >= 0);
+    REQUIRE(nIndex < GetSize());
 
-  void Copy(const CArray& src);
-  void RemoveAll();
+    return storage[nIndex];
+  }
 
-  const TYPE& operator[](INT_PTR nIndex) const;
-  TYPE& operator[](INT_PTR nIndex);
+  INT_PTR GetSize() const
+  {
+    return storage.size();
+  }
+
+  INT_PTR GetCount() const
+  {
+    return GetSize();
+  }
+
+  void SetSize(INT_PTR nNewSize, INT_PTR nGrowBy = -1)
+  {
+    REQUIRE(nNewSize >= 0);
+
+    storage.resize(nNewSize);
+  }
+
+  INT_PTR Add(ARG_TYPE newElement)
+  {
+    storage.push_back(newElement);
+
+    return storage.size() - 1;
+  }
+
+  void Copy(const CArray& src)
+  {
+    if (&src == this) return;
+
+    typename storage_type::size_type srcSize = src.storage.size();
+    typename storage_type::size_type dstSize = storage.size();
+    typename storage_type::size_type minSize = srcSize < dstSize ? srcSize : dstSize;
+
+    for (typename storage_type::size_type i = 0; i < minSize; i++)
+    {
+      storage[i] = src.storage[i];
+    }
+
+    if      (dstSize > srcSize)
+    {
+      storage.resize(srcSize);
+    }
+    else if (dstSize < srcSize)
+    {
+      for (typename storage_type::size_type i = dstSize; i < srcSize; i++)
+      {
+        storage.push_back(src.storage[i]);
+      }
+    }
+  }
+
+  void RemoveAll()
+  {
+    storage.clear();
+  }
+
+  const TYPE& operator[](INT_PTR nIndex) const
+  {
+    return GetAt(nIndex);
+  }
+
+  TYPE& operator[](INT_PTR nIndex)
+  {
+    return GetAt(nIndex);
+  }
 
   // Operations that move elements around
   // void InsertAt(INT_PTR nIndex, ARG_TYPE newElement, INT_PTR nCount = 1);
-  void RemoveAt(INT_PTR nIndex, INT_PTR nCount = 1);
+  void RemoveAt(INT_PTR nIndex, INT_PTR nCount = 1)
+  {
+    typename storage_type::size_type size = storage.size();
+
+    REQUIRE(nIndex >= 0);
+    REQUIRE(nIndex < size);
+
+    typename storage_type::size_type afterLastIndex = nIndex + nCount;
+
+    if (afterLastIndex > size)
+    {
+      //TODO: assert in debug mode
+
+      afterLastIndex = size;
+    }
+
+    storage.erase(nIndex,afterLastIndex);
+  }
   // void InsertAt(INT_PTR nStartIndex, CArray* pNewArray);
 
-  TYPE& ElementAt(INT_PTR nIndex);
+  TYPE& ElementAt(INT_PTR nIndex)
+  {
+    return GetAt(nIndex);
+  }
 };
 
 template<class TYPE, class ARG_TYPE = const TYPE&>
