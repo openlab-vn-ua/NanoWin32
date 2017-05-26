4cdf1dbda4197c0832ebf6cbdc1c17d01573e1c6
Sergey Prokhorchuk <s.stager@gmail.com>
2017-05-26 12:30:33 +0300

    NanoWinMFCAfxColTests.cpp module added (only trivial tests)

---
+++ tests/src/NanoWinMFCAfxColTests.cpp
@@ -0,0 +1,44 @@
+#include "NWUnitTest.h"
+
+#ifdef __linux
+ #include "NanoWinMFCAfxCol.h"
+ #include <time.h>
+#else
+ #include <windows.h>
+#endif
+
+NW_BEGIN_TEST_GROUP_SIMPLE(NanoWinMFCAfxColCArrayTestGroup)
+
+NW_TEST(NanoWinMFCAfxColCArrayTestGroup, CArrayIsEmptyByDefault)
+{
+  CArray<int> arr;
+
+  NW_CHECK_EQUAL_INTS(0,arr.GetCount());
+}
+
+NW_END_TEST_GROUP()
+
+
+NW_BEGIN_TEST_GROUP_SIMPLE(NanoWinMFCAfxColCListTestGroup)
+
+NW_TEST(NanoWinMFCAfxColCListTestGroup, CListIsEmptyByDefault)
+{
+  CList<int> lst;
+
+  NW_CHECK_TRUE(lst.IsEmpty());
+  NW_CHECK_EQUAL_INTS(0,lst.GetCount());
+}
+
+NW_END_TEST_GROUP()
+
+NW_BEGIN_TEST_GROUP_SIMPLE(NanoWinMFCAfxColCStringArrayTestGroup)
+
+NW_TEST(NanoWinMFCAfxColCStringArrayTestGroup, CStringArrayIsEmptyByDefault)
+{
+  CStringArray lst;
+
+  NW_CHECK_EQUAL_INTS(0,lst.GetCount());
+}
+
+NW_END_TEST_GROUP()
+
