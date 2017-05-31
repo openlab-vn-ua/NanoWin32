d10dcaeec8cf0f2943178c5eefd644f619ea6f90
Sergey Prokhorchuk <s.stager@gmail.com>
2017-05-31 16:50:23 +0300

    simple implementation of C...Exception::GetErrorMessage of methods added

--- src/NanoWinMFCAfx.cpp
+++ src/NanoWinMFCAfx.cpp
@@ -5,6 +5,7 @@
 
 // MFCAfx functions
 
+#include "NanoWinMsSafeString.h"
 #include "NanoWinMFCAfx.h"
 
 // implementation of abstract virtual destructor is required, without it it would be
@@ -24,24 +25,71 @@ const
 BOOL CMemoryException::GetErrorMessage(LPTSTR lpszError, UINT nMaxError, PUINT pnHelpContext)
 const
 {
-  // TODO: Implement me
-  lpszError[0] = 0;
-  return FALSE;
+  if (_tcscpy_s(lpszError, nMaxError, _T("not enough memory")) == 0)
+  {
+    if (pnHelpContext != NULL)
+    {
+      *pnHelpContext = 0;
+    }
+
+    return TRUE;
+  }
+  else
+  {
+    return FALSE;
+  }
 }
 
 BOOL CNotSupportedException::GetErrorMessage(LPTSTR lpszError, UINT nMaxError, PUINT pnHelpContext)
 const
 {
-  // TODO: Implement me
-  lpszError[0] = 0;
-  return FALSE;
+  if (_tcscpy_s(lpszError, nMaxError, _T("not supported")) == 0)
+  {
+    if (pnHelpContext != NULL)
+    {
+      *pnHelpContext = 0;
+    }
+
+    return TRUE;
+  }
+  else
+  {
+    return FALSE;
+  }
 }
 
 BOOL CInvalidArgException::GetErrorMessage(LPTSTR lpszError, UINT nMaxError, PUINT pnHelpContext)
 const
 {
-  // TODO: Implement me
-  lpszError[0] = 0;
-  return FALSE;
+  if (_tcscpy_s(lpszError, nMaxError, _T("invalid argument")) == 0)
+  {
+    if (pnHelpContext != NULL)
+    {
+      *pnHelpContext = 0;
+    }
+
+    return TRUE;
+  }
+  else
+  {
+    return FALSE;
+  }
 }
 
+BOOL CFileException::GetErrorMessage(LPTSTR lpszError, UINT nMaxError, PUINT pnHelpContext)
+const
+{
+  if (_tcscpy_s(lpszError, nMaxError, _T("file error")) == 0)
+  {
+    if (pnHelpContext != NULL)
+    {
+      *pnHelpContext = 0;
+    }
+
+    return TRUE;
+  }
+  else
+  {
+    return FALSE;
+  }
+}
