87bfd0caf1ff7731c68dc19b47dc5c5037dc19b0
Anastasia Tkachuk <1pi15b.tkachuk@gmail.com>
2017-06-09 15:25:14 +0300

    added tests for sscanf_s and fscanf_s

---
+++ tests/src/NanoWinMsSafeScanfTests.cpp
@@ -0,0 +1,599 @@
+#include "NWUnitTest.h"
+
+#include <stdio.h>
+
+#ifdef __linux
+#include <string.h>
+
+#else
+#include <windows.h>
+#endif
+
+namespace
+{
+  class ScanfTestFile
+  {
+  public:
+
+    ScanfTestFile(const char *testStr)
+    {
+      stream = fopen(fileName, "w+t");
+
+      if (stream != NULL)
+      {
+        fputs(testStr, stream);
+        fseek(stream, 0, SEEK_SET);
+      }
+    }
+
+    ~ScanfTestFile()
+    {
+      if (stream != NULL)
+      {
+        fclose(stream);
+      }
+
+      unlink(fileName);
+    }
+
+    FILE *getStream()
+    {
+      return stream;
+    }
+
+    bool isOk()
+    {
+      return stream != NULL;
+    }
+
+  private:
+
+    FILE *stream;
+
+    static constexpr const char *fileName = "fscanf_s_test.txt";
+  };
+}
+
+
+NW_BEGIN_TEST_GROUP_SIMPLE(NanoWinSafeScanfTestGroup)
+
+#define NW_DEFINE_SIMPLE_TYPED_SSCANF_TEST(type,expected_value,specifier,testname)  \
+NW_TEST(NanoWinSafeScanfTestGroup, testname)                                        \
+{                                                                                   \
+	const char  input[] = #expected_value;                                            \
+	type                  actual_value;                                               \
+                                                                                    \
+	sscanf_s(input, specifier, &actual_value);                                        \
+                                                                                    \
+	NW_CHECK_EQUAL((type)expected_value, (type)actual_value);                         \
+}
+
+NW_DEFINE_SIMPLE_TYPED_SSCANF_TEST(int,          123,   "%d",  SScanfSimpleIntTest)
+NW_DEFINE_SIMPLE_TYPED_SSCANF_TEST(int,          0123,  "%od", SScanfSimpleOctIntTest)
+NW_DEFINE_SIMPLE_TYPED_SSCANF_TEST(unsigned int, 0x123, "%xd", SScanfSimpleHexIntTest)
+
+NW_DEFINE_SIMPLE_TYPED_SSCANF_TEST(float, 1.23, "%f", SScanfSimpleFloatFTest)
+NW_DEFINE_SIMPLE_TYPED_SSCANF_TEST(float, 1.23, "%e", SScanfSimpleFloatETest)
+NW_DEFINE_SIMPLE_TYPED_SSCANF_TEST(float, 1.23, "%g", SScanfSimpleFloatGTest)
+
+NW_DEFINE_SIMPLE_TYPED_SSCANF_TEST(long int,           345, "%ld",  SScanfSimpleLongTest)
+NW_DEFINE_SIMPLE_TYPED_SSCANF_TEST(unsigned long,      345, "%lu",  SScanfSimpleLongUIntTest)
+NW_DEFINE_SIMPLE_TYPED_SSCANF_TEST(unsigned long long, 345, "%llu", SScanfSimpleLongLongUIntTest)
+
+NW_DEFINE_SIMPLE_TYPED_SSCANF_TEST(short int, 456, "%hi", SScanfSimpleShortTest)
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfSimpleLongLongIntTest)
+{
+  const char  input[] = "345";
+  long long   longNum;
+
+  int count = sscanf_s(input, "%lld", &longNum);
+
+  NW_CHECK_EQUAL_INTS (1,   count);
+  NW_CHECK_EQUAL_LONGS(345, longNum);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfSimpleShortUIntTest)
+{
+  const char  input[] = "34";
+  unsigned short   intNum;
+
+  int count = sscanf_s(input, "%hu", &intNum);
+
+  NW_CHECK_EQUAL_INTS (1,  count);
+  NW_CHECK_EQUAL_LONGS(34, intNum);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfInvalidValueIntTest)
+{
+	const char  input[] = "abc";
+	int   intNum;
+
+	int count = sscanf_s(input, "%d", &intNum);
+
+	NW_CHECK_EQUAL_INTS(0, count);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfIntWithCounterTest)
+{
+	const char  input[] = "-123";
+	int   intNum;
+
+	int count = sscanf_s(input, "%3d", &intNum);
+
+	NW_CHECK_EQUAL_INTS(1, count);
+	NW_CHECK_EQUAL_INTS(-12, intNum);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfIntTest)
+{
+	const char  input[] = "0230  -0045 123abc";
+	int   intNum1;
+	int   intNum2;
+	int   intNum3;
+
+	int count = sscanf_s(input, "%10d%d%d", &intNum1, &intNum2, &intNum3);
+
+	NW_CHECK_EQUAL_INTS(3, count);
+
+	NW_CHECK_EQUAL_INTS(230, intNum1);
+	NW_CHECK_EQUAL_INTS(-45,  intNum2);
+	NW_CHECK_EQUAL_INTS(123, intNum3);
+
+	count = sscanf_s(input, "%d%2d", &intNum1, &intNum2);
+
+	NW_CHECK_EQUAL_INTS(2, count);
+
+	NW_CHECK_EQUAL_INTS(230, intNum1);
+	NW_CHECK_EQUAL_INTS(0, intNum2);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfInvalidValueOctIntTest)
+{
+	const char  input[] = "abc";
+	int   octNum;
+
+	int count = sscanf_s(input, "%od", &octNum);
+
+	NW_CHECK_EQUAL_INTS(0, count);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfOctIntWithCounterTest)
+{
+	const char  input[] = "-123";
+	int   octNum;
+
+	int count = sscanf_s(input, "%3od", &octNum);
+
+	NW_CHECK_EQUAL_INTS(1,    count);
+	NW_CHECK_EQUAL_INTS(-012, octNum);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfOctIntTest)
+{
+	const char  input[] = "0230  -00459 123abc";
+	int   octNum1;
+	int   octNum2;
+	int   octNum3;
+
+	int count = sscanf_s(input, "%10od%od%od", &octNum1, &octNum2, &octNum3);
+
+	NW_CHECK_EQUAL_INTS(1, count);
+
+	NW_CHECK_EQUAL_INTS(0230, octNum1);
+
+	count = sscanf_s(input, "%od%5od", &octNum1, &octNum2);
+
+	NW_CHECK_EQUAL_INTS(1, count);
+
+	NW_CHECK_EQUAL_INTS(0230, octNum1);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfInvalidValueHexIntTest)
+{
+	const char   input[] = "www";
+	unsigned int hexNum;
+
+	int count = sscanf_s(input, "%xd", &hexNum);
+
+	NW_CHECK_EQUAL_INTS(0x0, count);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfHexIntWithCounterTest)
+{
+	const char   input[] = "12AB";
+	unsigned int hexNum;
+
+	int count = sscanf_s(input, "%3xd", &hexNum);
+
+	NW_CHECK_EQUAL_INTS(0x1, count);
+	NW_CHECK_EQUAL_INTS(0x12A, hexNum);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfHexIntTest)
+{
+	const char   input[] = "0230  -0045 123ABX";
+	unsigned int hexNum1;
+	unsigned int hexNum2;
+	unsigned int hexNum3;
+
+	int count = sscanf_s(input, "%10xd%xd%xd", &hexNum1, &hexNum2, &hexNum3);
+
+	NW_CHECK_EQUAL_INTS(1, count);
+
+	NW_CHECK_EQUAL_INTS(0x230,   hexNum1);
+
+	count = sscanf_s(input, "%xd%2xd", &hexNum1, &hexNum2);
+
+	NW_CHECK_EQUAL_INTS(1, count);
+
+	NW_CHECK_EQUAL_INTS(0x230, hexNum1);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfInvalidValueShortTest)
+{
+	const char  input[] = "abc";
+	short       shortNum;
+
+	int count = sscanf_s(input, "%hd", &shortNum);
+
+	NW_CHECK_EQUAL_INTS(0, count);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfShortWithCounterTest)
+{
+	const char  input[] = "-123";
+	short       shortNum;
+
+	int count = sscanf_s(input, "%3hd", &shortNum);
+
+	NW_CHECK_EQUAL_INTS(1, count);
+	NW_CHECK_EQUAL_INTS(-12, shortNum);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfShortTest)
+{
+	const char  input[] = "0230  -0045 123abc";
+	short       shortNum1;
+	short       shortNum2;
+	short       shortNum3;
+
+	int count = sscanf_s(input, "%10hd%hd%hd", &shortNum1, &shortNum2, &shortNum3);
+
+	NW_CHECK_EQUAL_INTS(3, count);
+
+	NW_CHECK_EQUAL_INTS(230, shortNum1);
+	NW_CHECK_EQUAL_INTS(-45, shortNum2);
+	NW_CHECK_EQUAL_INTS(123, shortNum3);
+
+	count = sscanf_s(input, "%hd%2hd", &shortNum1, &shortNum2);
+
+	NW_CHECK_EQUAL_INTS(2, count);
+
+	NW_CHECK_EQUAL_INTS(230, shortNum1);
+	NW_CHECK_EQUAL_INTS(0, shortNum2);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfInvalidValueLongTest)
+{
+	const char  input[] = "abc";
+	long int    longNum;
+
+	int count = sscanf_s(input, "%ld", &longNum);
+
+	NW_CHECK_EQUAL_INTS(0, count);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfLongWithCounterTest)
+{
+	const char  input[] = "123";
+	long int    longNum;
+
+	int count = sscanf_s(input, "%2ld", &longNum);
+
+	NW_CHECK_EQUAL_INTS(1, count);
+	NW_CHECK_EQUAL_LONGS(12, longNum);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfLongTest)
+{
+	const char  input[] = "012 -00045  12.3abc";
+	long int    longNum1;
+	long int    longNum2;
+	long int    longNum3;
+
+	int count = sscanf_s(input, "%5ld%ld%ld", &longNum1, &longNum2, &longNum3);
+
+	NW_CHECK_EQUAL_INTS(3, count);
+
+	NW_CHECK_EQUAL_LONGS(12, longNum1);
+	NW_CHECK_EQUAL_LONGS(-45, longNum2);
+	NW_CHECK_EQUAL_LONGS(12, longNum3);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfInvalidValueFloatTest)
+{
+	const char  input[] = "abc";
+	float floatNum;
+
+	int count = sscanf_s(input, "%f", &floatNum);
+
+	NW_CHECK_EQUAL_INTS(0, count);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfFloatWithCounterTest)
+{
+	const char  input[] = "1.23";
+	float floatNum;
+
+	int count = sscanf_s(input, "%3f", &floatNum);
+
+	NW_CHECK_EQUAL_INTS(1, count);
+	NW_CHECK_EQUAL(1.2f, floatNum);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfFloatTest)
+{
+	const char  input[] = "1.230045 abc12.3abc";
+	float floatNum1;
+	float floatNum2;
+	float floatNum3;
+
+	int count = sscanf_s(input, "%4f%6f%f", &floatNum1, &floatNum2, &floatNum3);
+
+	NW_CHECK_EQUAL_INTS(2, count);
+
+	NW_CHECK_EQUAL(1.23f, floatNum1);
+	NW_CHECK_EQUAL(45.0f, floatNum2);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfIntAndFloatTest)
+{
+	const char  input[] = "123.45";
+	int   intNum1;
+	float floatNum1;
+
+	int count = sscanf_s(input, "%2d%6f", &intNum1, &floatNum1);
+
+	NW_CHECK_EQUAL_INTS(2, count);
+
+	NW_CHECK_EQUAL_INTS(12,    intNum1);
+	NW_CHECK_EQUAL_INTS(3.45f, floatNum1);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfStrSimpleTest)
+{
+	const char  input[] = "123abc";
+	char str[8];
+
+	int count = sscanf_s(input, "%s", str, sizeof(str));
+
+	NW_CHECK_EQUAL_INTS(1, count);
+	NW_CHECK_EQUAL_STRCMP("123abc", str);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfStrWithCounterTest)
+{
+	const char  input[] = "123abc	 def 456";
+	char str[8];
+
+	int count = sscanf_s(input, "%3s", str, sizeof(str));
+
+	NW_CHECK_EQUAL_INTS(1, count);
+	NW_CHECK_EQUAL_STRCMP("123", str);
+
+	count = sscanf_s(input, "%10s", str, sizeof(str));
+
+	NW_CHECK_EQUAL_INTS(1, count);
+	NW_CHECK_EQUAL_STRCMP("123abc", str);
+}
+/*
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfStrSmallBuffTest)
+{
+	const char  input[] = "123abc";
+	char str[3];
+
+	int count = sscanf_s(input, "%s", str, sizeof(str));
+
+	NW_CHECK_EQUAL_INTS(0, count);
+	NW_CHECK_EQUAL_STRCMP("", str);
+}
+*/
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfStrTest)
+{
+	const char  input[] = "123  abc		4def5";
+	
+	char str1[8];
+	char str2[8];
+	char str3[8];
+
+	int count = sscanf_s(input, "%s%s%s", str1, sizeof(str1), str2, sizeof(str2), str3, sizeof(str3));
+
+	NW_CHECK_EQUAL_INTS(3, count);
+
+	NW_CHECK_EQUAL_STRCMP("123", str1);
+	NW_CHECK_EQUAL_STRCMP("abc", str2);
+	NW_CHECK_EQUAL_STRCMP("4def5", str3);
+
+	count = sscanf_s(input, "%2s%s%s", str1, sizeof(str1), str2, sizeof(str2), str3, sizeof(str3));
+
+	NW_CHECK_EQUAL_INTS(3, count);
+
+	NW_CHECK_EQUAL_STRCMP("12", str1);
+	NW_CHECK_EQUAL_STRCMP("3", str2);
+	NW_CHECK_EQUAL_STRCMP("abc", str3);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfCharSimpleTest)
+{
+	const char  input[] = "abc";
+	char symb;
+
+	int count = sscanf_s(input, "%c", &symb, 1);
+
+	NW_CHECK_EQUAL_INTS(1, count);
+	NW_CHECK_EQUAL_STRCMP('a', symb);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfCharWithCounterTest)
+{
+	const char  input[] = "abc";
+	char symb[4];
+
+	int count = sscanf_s(input, "%3c", symb, 3);
+
+	NW_CHECK_EQUAL_INTS(1, count);
+
+	NW_CHECK_EQUAL('a', symb[0]);
+	NW_CHECK_EQUAL('b', symb[1]);
+	NW_CHECK_EQUAL('c', symb[2]);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfCharTest)
+{
+	const char  input[] = "abc de";
+	char symb;
+	char str[5];
+
+	int count = sscanf_s(input, "%c%3c", &symb, 1, str, 3);
+
+	NW_CHECK_EQUAL_INTS(2, count);
+
+	NW_CHECK_EQUAL('a', symb);
+	NW_CHECK_EQUAL('b', str[0]);
+	NW_CHECK_EQUAL('c', str[1]);
+	NW_CHECK_EQUAL(' ', str[2]);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfCharAndIntTest)
+{
+	const char  input[] = "a12";
+	char symb;
+	int  intNum;
+
+	int count = sscanf_s(input, "%c%d", &symb, 1, &intNum);
+
+	NW_CHECK_EQUAL_INTS(2, count);
+
+	NW_CHECK_EQUAL('a', symb);
+	NW_CHECK_EQUAL_INTS(12, intNum);
+}
+
+NW_TEST(NanoWinSafeScanfTestGroup, SScanfCommonTest)
+{
+	const char  input[] = "abc123 4def5.6.7 nnn56.78";
+	
+	char  str1[10];
+	char  str2[10];
+	char  str3[10];
+	
+  int   intNum1;
+	float floatNum1;
+	
+	int count = sscanf_s(input, "%s%d%s%s", str1, sizeof(str1), &intNum1, str2, sizeof(str2), str3, sizeof(str3));
+
+	NW_CHECK_EQUAL_INTS(4, count);
+
+	NW_CHECK_EQUAL_STRCMP("abc123", str1);
+	NW_CHECK_EQUAL_INTS  (4,     intNum1);
+	NW_CHECK_EQUAL_STRCMP("def5.6.7", str2);
+	NW_CHECK_EQUAL_STRCMP("nnn56.78", str3);
+
+	count = sscanf_s(input, "%3s%d%4s%f", str1, sizeof(str1), &intNum1, str2, sizeof(str2), &floatNum1);
+
+	NW_CHECK_EQUAL_INTS(4, count);
+
+	NW_CHECK_EQUAL_STRCMP("abc", str1);
+	NW_CHECK_EQUAL_INTS  (123, intNum1);
+	NW_CHECK_EQUAL_STRCMP("4def", str2);
+	NW_CHECK_EQUAL       (5.6f, floatNum1);
+}
+
+NW_END_TEST_GROUP()
+
+
+NW_BEGIN_TEST_GROUP_SIMPLE(NanoWinSafeFScanfTestGroup)
+
+NW_TEST(NanoWinSafeFScanfTestGroup, FScanfFileEmptyTest)
+{
+  ScanfTestFile input("");
+  int           intNum;
+
+  int count = fscanf_s(input.getStream(), "%d", &intNum);
+
+  NW_CHECK_EQUAL_INTS(-1, count);
+}
+
+NW_TEST(NanoWinSafeFScanfTestGroup, FScanfIntTest)
+{
+  ScanfTestFile input("123");
+  int           intNum;
+
+  int count = fscanf_s(input.getStream(), "%d", &intNum);
+
+  NW_CHECK_EQUAL_INTS(1, count);
+  NW_CHECK_EQUAL_INTS(123, intNum);
+}
+
+NW_TEST(NanoWinSafeFScanfTestGroup, FScanfIntTest1)
+{
+  ScanfTestFile input("123");
+  int           intNum1;
+  int           intNum2;
+
+  int count = fscanf_s(input.getStream(), "%d%d", &intNum1, &intNum2);
+
+  NW_CHECK_EQUAL_INTS(1, count);
+  NW_CHECK_EQUAL_INTS(123, intNum1);
+}
+
+NW_TEST(NanoWinSafeFScanfTestGroup, FScanfIntTest2)
+{
+  ScanfTestFile input("123      ");
+  int           intNum1;
+  int           intNum2;
+
+  int count = fscanf_s(input.getStream(), "%d%d", &intNum1, &intNum2);
+
+  NW_CHECK_EQUAL_INTS(1, count);
+  NW_CHECK_EQUAL_INTS(123, intNum1);
+}
+
+NW_TEST(NanoWinSafeFScanfTestGroup, FScanfIntTest3)
+{
+  ScanfTestFile input("123");
+  int           intNum1;
+  int           intNum2;
+
+  int count = fscanf_s(input.getStream(), "%2d%3d", &intNum1, &intNum2);
+
+  NW_CHECK_EQUAL_INTS(2,  count);
+  NW_CHECK_EQUAL_INTS(12, intNum1);
+  NW_CHECK_EQUAL_INTS(3,  intNum2);
+}
+
+NW_TEST(NanoWinSafeFScanfTestGroup, FScanfStrTest1)
+{
+  ScanfTestFile input("123");
+  char          str1[8];
+  char          str2[8];
+
+  int count = fscanf_s(input.getStream(), "%s%s", str1, sizeof(str1), str2, sizeof(str2));
+
+  NW_CHECK_EQUAL_INTS(1, count);
+  NW_CHECK_EQUAL_STRCMP("123", str1);
+}
+
+NW_TEST(NanoWinSafeFScanfTestGroup, FScanfStrTest2)
+{
+  ScanfTestFile input("123      ");
+  char          str1[8];
+  char          str2[8];
+
+  int count = fscanf_s(input.getStream(), "%4s%s", str1, sizeof(str1), str2, sizeof(str2));
+
+  NW_CHECK_EQUAL_INTS(1, count);
+  NW_CHECK_EQUAL_STRCMP("123", str1);
+}
+
+NW_END_TEST_GROUP()
