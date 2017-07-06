35b407728c241b94ffa309ad2a139d320af99d5e
Anastasia Tkachuk <1pi15b.tkachuk@gmail.com>
2017-07-06 13:54:57 +0300

    added tests for NanoWinMsFWPrintf function and fixed bugs in tests in NanoWinMsPrintfTests module

--- tests/src/NanoWinMsPrintfTests.cpp
+++ tests/src/NanoWinMsPrintfTests.cpp
@@ -2,13 +2,55 @@
 
 #ifdef __linux
  #include "NanoWinMsPrintf.h"
+ #include <unistd.h>
 #else
  #include <windows.h>
 #endif
 
-NW_BEGIN_TEST_GROUP_SIMPLE(NanoWinMsPrintfTestGroup)
+#define FILE_NAME     ("testFile.txt")
+#define FILE_NAME_W   (L"testFile.txt")
 
-NW_TEST(NanoWinMsPrintfTestGroup, PrintfWFormatMs2UnixSimpleTest)
+static FILE *GetFile()
+{
+  #ifndef __GNUC__ 
+  FILE *stream = _wfopen(FILE_NAME_W, L"w+t");
+  #else
+  FILE *stream = fopen(FILE_NAME, "w+t");
+  #endif
+
+  if (stream != NULL)
+  {
+    return stream;
+  }
+  else return NULL;
+}
+
+static void CloseFile(FILE *stream)
+{
+  if (stream != NULL)
+  {
+    fclose(stream);
+  }
+  #ifndef __GNUC__  
+  _wunlink(FILE_NAME_W);
+  #else
+  unlink(FILE_NAME);
+  #endif
+}
+
+static void ReadFile(FILE *stream, wchar_t *buff)
+{
+  fseek(stream, 0, SEEK_SET);
+
+  if (stream != NULL)
+  {
+    fgetws(buff, 256, stream);
+  }
+}
+
+NW_BEGIN_TEST_GROUP_SIMPLE(PrintfWFormatMs2UnixTestGroup)
+
+NW_TEST(PrintfWFormatMs2UnixTestGroup, PrintfWFormatMs2UnixSimpleTest)
 {
   const wchar_t *srcS  = L"%s";
   wchar_t       destS[256];
@@ -50,16 +92,16 @@ NW_TEST(NanoWinMsPrintfTestGroup, PrintfWFormatMs2UnixSimpleTest)
   wchar_t       destWS[256];
 
   NanoWinMsPrintfWFormatMs2Unix(destWS, srcWS);
-  NW_CHECK_EQUAL_MEMCMP(L"%ws", destWS, 4 * sizeof(wchar_t));
+  NW_CHECK_EQUAL_MEMCMP(L"%ls", destWS, 4 * sizeof(wchar_t));
 
   const wchar_t *srcWC = L"%wc";
   wchar_t       destWC[256];
 
   NanoWinMsPrintfWFormatMs2Unix(destWC, srcWC);
-  NW_CHECK_EQUAL_MEMCMP(L"%wc", destWC, 4 * sizeof(wchar_t));
+  NW_CHECK_EQUAL_MEMCMP(L"%lc", destWC, 4 * sizeof(wchar_t));
 }
 
-NW_TEST(NanoWinMsPrintfTestGroup, PrintfWFormatMs2UnixTest)
+NW_TEST(PrintfWFormatMs2UnixTestGroup, PrintfWFormatMs2UnixTest)
 {
   const wchar_t *srcS = L"%5s %#20S";
   wchar_t       destS[256];
@@ -83,7 +125,7 @@ NW_TEST(NanoWinMsPrintfTestGroup, PrintfWFormatMs2UnixTest)
   wchar_t       destC[256];
 
   NanoWinMsPrintfWFormatMs2Unix(destC, srcC);
-  NW_CHECK_EQUAL_MEMCMP(L"%-C %5c %wc", destC, 12 * sizeof(wchar_t));
+  NW_CHECK_EQUAL_MEMCMP(L"%-C %5c %lc", destC, 12 * sizeof(wchar_t));
 
   const wchar_t *srcG = L"%-3lg %+5LG";
   wchar_t       destG[256];
@@ -98,83 +140,193 @@ NW_TEST(NanoWinMsPrintfTestGroup, PrintfWFormatMs2UnixTest)
   NW_CHECK_EQUAL_MEMCMP(L"%-*3.a   %+5#A", destA, 15 * sizeof(wchar_t));
 }
 
-NW_TEST(NanoWinMsPrintfTestGroup, NanoWinMsSWPrintfSimpleTest)
+NW_END_TEST_GROUP()
+
+NW_BEGIN_TEST_GROUP_SIMPLE(NanoWinMsSWPrintfTestGroup)
+
+NW_TEST(NanoWinMsSWPrintfTestGroup, NanoWinMsSWPrintfSimpleTest)
 {
   wchar_t str[256];
 
   int count1 = NanoWinMsSWPrintf(str, 4, L"%s", L"abc");
-  NW_CHECK_EQUAL(count1, 3);
+  NW_CHECK_EQUAL(3, count1);
   NW_CHECK_EQUAL_MEMCMP(L"abc", str, 4 * sizeof(wchar_t));
-  #ifndef __GNUC__
+
   int count2 = NanoWinMsSWPrintf(str, 4, L"%ws", L"def");
-  NW_CHECK_EQUAL(count2, 3);
+  NW_CHECK_EQUAL(3, count2);
   NW_CHECK_EQUAL_MEMCMP(L"def", str, 4 * sizeof(wchar_t));
 
   int count3 = NanoWinMsSWPrintf(str, 4, L"%wc", L'g');
-  NW_CHECK_EQUAL(count3, 1);
+  NW_CHECK_EQUAL(1, count3);
   NW_CHECK_EQUAL_MEMCMP(L"g", str, 2 * sizeof(wchar_t));
-  #endif
-  int count4 = NanoWinMsSWPrintf(str, 4, L"%hd", -12);
-  NW_CHECK_EQUAL(count4, 3);
+
+  int count4 = NanoWinMsSWPrintf(str, 4, L"%hd", (short int)-12);
+  NW_CHECK_EQUAL(3, count4);
   NW_CHECK_EQUAL_MEMCMP(L"-12", str, 4 * sizeof(wchar_t));
 
-  int count5 = NanoWinMsSWPrintf(str, 4, L"%lu", 34);
-  NW_CHECK_EQUAL(count5, 2);
+  int count5 = NanoWinMsSWPrintf(str, 4, L"%lu", (unsigned long int)34);
+  NW_CHECK_EQUAL(2, count5);
   NW_CHECK_EQUAL_MEMCMP(L"34", str, 3 * sizeof(wchar_t));
 
   int count6 = NanoWinMsSWPrintf(str, 4, L"%c", L'd');
-  NW_CHECK_EQUAL(count6, 1);
+  NW_CHECK_EQUAL(1, count6);
   NW_CHECK_EQUAL_MEMCMP(L"d", str, 2 * sizeof(wchar_t));
 
   int count7 = NanoWinMsSWPrintf(str, 5, L"%#x", 0xFF);
-  NW_CHECK_EQUAL(count7, 4);
+  NW_CHECK_EQUAL(4, count7);
   NW_CHECK_EQUAL_MEMCMP(L"0xff", str, 5 * sizeof(wchar_t));
 
   int count8 = NanoWinMsSWPrintf(str, 5, L"%#X", 0xff);
-  NW_CHECK_EQUAL(count8, 4);
+  NW_CHECK_EQUAL(4, count8);
   NW_CHECK_EQUAL_MEMCMP(L"0XFF", str, 5 * sizeof(wchar_t));
 
   int count9 = NanoWinMsSWPrintf(str, 5, L"%o", 056);
-  NW_CHECK_EQUAL(count9, 2);
+  NW_CHECK_EQUAL(2, count9);
   NW_CHECK_EQUAL_MEMCMP(L"56", str, 3 * sizeof(wchar_t));
 
-  int count10 = NanoWinMsSWPrintf(str, 10, L"%f", 5.678);
-  NW_CHECK_EQUAL(count10, 8);
+  int count10 = NanoWinMsSWPrintf(str, 10, L"%f", 5.678f);
+  NW_CHECK_EQUAL(8, count10);
   NW_CHECK_EQUAL_MEMCMP(L"5.678000", str, 9 * sizeof(wchar_t));
 }
 
-NW_TEST(NanoWinMsPrintfTestGroup, NanoWinMsSWPrintfTest)
+NW_TEST(NanoWinMsSWPrintfTestGroup, NanoWinMsSWPrintfTest)
 {
   wchar_t str[256];  
-  #ifndef __GNUC__
+
   int count1 = NanoWinMsSWPrintf(str, sizeof(str) / sizeof(wchar_t), L"%ls ww %#2s%ws", L"abc", L"f", L"zz");
-  NW_CHECK_EQUAL(count1, 11);
+  NW_CHECK_EQUAL(11, count1);
   NW_CHECK_EQUAL_MEMCMP(L"abc ww  fzz", str, 12 * sizeof(wchar_t));
 
   int count2 = NanoWinMsSWPrintf(str, sizeof(str) / sizeof(wchar_t), L"%c %wc %lc", L'd', L'e', L'f');
-  NW_CHECK_EQUAL(count2, 5);
+  NW_CHECK_EQUAL(5, count2);
   NW_CHECK_EQUAL_MEMCMP(L"d e f", str, 6 * sizeof(wchar_t));
-  #endif
   
-  int count3 = NanoWinMsSWPrintf(str, sizeof(str) / sizeof(wchar_t), L"%hd %+i  bb %#llu", -56, 123, 10);
-  NW_CHECK_EQUAL(count3, 15);
+  int count3 = NanoWinMsSWPrintf(str, sizeof(str) / sizeof(wchar_t), L"%hd %+i  bb %#llu", (short int)-56, 123, (long long unsigned int)10);
+  NW_CHECK_EQUAL(15, count3);
   NW_CHECK_EQUAL_MEMCMP(L"-56 +123  bb 10", str, 16 * sizeof(wchar_t));
 
   int count4 = NanoWinMsSWPrintf(str, sizeof(str) / sizeof(wchar_t), L"%#x  %#X %x", 11, 22, 33);
-  NW_CHECK_EQUAL(count4, 12);
+  NW_CHECK_EQUAL(12, count4);
   NW_CHECK_EQUAL_MEMCMP(L"0xb  0X16 21", str, 13 * sizeof(wchar_t));
 
   int count5 = NanoWinMsSWPrintf(str, sizeof(str) / sizeof(wchar_t), L"%o  %#o", 056, 24);
-  NW_CHECK_EQUAL(count5, 7);
+  NW_CHECK_EQUAL(7, count5);
   NW_CHECK_EQUAL_MEMCMP(L"56  030", str, 8 * sizeof(wchar_t));
 
-  int count6 = NanoWinMsSWPrintf(str, sizeof(str) / sizeof(wchar_t), L"%o  %#o", 056, 24);
-  NW_CHECK_EQUAL(count6, 7);
-  NW_CHECK_EQUAL_MEMCMP(L"56  030", str, 8 * sizeof(wchar_t));
-
-  int count7 = NanoWinMsSWPrintf(str, sizeof(str) / sizeof(wchar_t), L"%.*f %.3f", 3, 0.1312, 1.22222);
-  NW_CHECK_EQUAL(count7, 11);
+  int count6 = NanoWinMsSWPrintf(str, sizeof(str) / sizeof(wchar_t), L"%.*f %.3f", 3, 0.1312f, 1.22222f);
+  NW_CHECK_EQUAL(11, count6);
   NW_CHECK_EQUAL_MEMCMP(L"0.131 1.222", str, 12 * sizeof(wchar_t));
+}
+
+NW_END_TEST_GROUP()
+
+NW_BEGIN_TEST_GROUP_SIMPLE(NanoWinMsFWPrintfTestGroup)
+#ifndef __GNUC__
+NW_TEST(NanoWinMsFWPrintfTestGroup, NanoWinMsFWPrintfSTest)
+{
+  wchar_t str[256];
+
+  FILE *file = GetFile();
+  if (file != NULL)
+  {
+    int count = NanoWinMsFWPrintf(file, L"%ls ww %#2s%ws", L"abc", L"f", L"zz");
+    NW_CHECK_EQUAL(11, count);
+  
+    ReadFile(file, str);
+    NW_CHECK_EQUAL_MEMCMP(L"abc ww  fzz", str, 12 * sizeof(wchar_t));
+   
+    CloseFile(file);
+  }
+}
+
+NW_TEST(NanoWinMsFWPrintfTestGroup, NanoWinMsFWPrintfCTest)
+{
+  wchar_t str[256];
+
+  FILE *file = GetFile();
+  
+  if (file != NULL)
+  {
+    int count = NanoWinMsFWPrintf(file, L"%c %wc %lc", L'd', L'e', L'f');
+    NW_CHECK_EQUAL(5, count);
+
+    ReadFile(file, str);
+    NW_CHECK_EQUAL_MEMCMP(L"d e f", str, 6 * sizeof(wchar_t));
+
+    CloseFile(file);
+  }
+}
+#endif
+NW_TEST(NanoWinMsFWPrintfTestGroup, NanoWinMsFWPrintfDTest)
+{
+  wchar_t str[256];
+
+  FILE *file = GetFile();
+
+  if (file != NULL)
+  {
+    int count = NanoWinMsFWPrintf(file, L"%hd %+i  bb %#llu", (short int)-56, 123, (long long unsigned int)10);
+    NW_CHECK_EQUAL(15, count);
+
+    ReadFile(file, str);
+    NW_CHECK_EQUAL_MEMCMP(L"-56 +123  bb 10", str, 16 * sizeof(wchar_t));
+
+    CloseFile(file);
+  }
+}
+
+NW_TEST(NanoWinMsFWPrintfTestGroup, NanoWinMsFWPrintfXTest)
+{
+  wchar_t str[256];
+
+  FILE *file = GetFile();
+
+  if (file != NULL)
+  {
+    int count = NanoWinMsFWPrintf(file, L"%#x  %#X %x", 11, 22, 33);
+    NW_CHECK_EQUAL(12, count);
+
+    ReadFile(file, str);
+    NW_CHECK_EQUAL_MEMCMP(L"0xb  0X16 21", str, 13 * sizeof(wchar_t));
+
+    CloseFile(file);
+  }
+}
+
+NW_TEST(NanoWinMsFWPrintfTestGroup, NanoWinMsFWPrintfOTest)
+{
+  wchar_t str[256];
+
+  FILE *file = GetFile();
+
+  if (file != NULL)
+  {
+    int count = NanoWinMsFWPrintf(file, L"%o  %#o", 056, 24);
+    NW_CHECK_EQUAL(7, count);
+
+    ReadFile(file, str);
+    NW_CHECK_EQUAL_MEMCMP(L"56  030", str, 8 * sizeof(wchar_t));
+
+    CloseFile(file);
+  }
+}
+
+NW_TEST(NanoWinMsFWPrintfTestGroup, NanoWinMsFWPrintfFTest)
+{
+  wchar_t str[256];
+
+  FILE *file = GetFile();
+
+  if (file != NULL)
+  {
+    int count = NanoWinMsFWPrintf(file, L"%.*f %.3f", 3, 0.1312f, 1.22222f);
+    NW_CHECK_EQUAL(11, count);
+
+    ReadFile(file, str);
+    NW_CHECK_EQUAL_MEMCMP(L"0.131 1.222", str, 12 * sizeof(wchar_t));
+
+    CloseFile(file);
+  }
 }
 
 NW_END_TEST_GROUP()
