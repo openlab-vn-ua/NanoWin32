5d9d2349ca551766213f5e2dcdbed4cc43c91052
S.Ageyev <justboss777@gmail.com>
2020-06-02 22:48:57 +0300

    Added unit test to check pure C modules compilation

---
+++ tests/src/NanoWinCompileTest.c
@@ -0,0 +1,14 @@
+// This file just tests that all headers are compiled in C mode
+
+#ifndef __linux
+// #include <windows.h>
+#endif
+
+#include "NanoWin.h"
+
+#include "NanoWinTCHAR.h"
+#include "NanoWinMsExtra.h"
+#include "NanoWinMsSafeString.h"
+#include "NanoWinMsSafePrintf.h"
+#include "NanoWinMsSafeScanf.h"
+
